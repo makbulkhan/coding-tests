1722fb57d72f335a42e5fddc175aea8e5785c557
Makbul Khan <kmakbul@nisum.com>
2024-09-07 03:59:04 +0530

    Coding assignment

--- front-end/tailwind.config.js
+++ front-end/tailwind.config.js
@@ -1,8 +1,22 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ['./index.html', './src/**/*.{html,js}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'clr-free': '#0db0c0',
+        'clr-standard': '#0069cc',
+        'clr-pro': '#ff8853',
+        'clr-enterprise': '#2c2c2c',
+        'clr-btn': '#01818f',
+        'clr-footer': '#2c2c2c',
+        'clr-border': '#e5e7e6',
+        'clr-gray': '#faf8f9',
+      },
+      fontFamily: {
+        'soehne': ['Soehne', 'Corbel', 'Arial']
+      }
+    },
   },
   plugins: [],
 }
